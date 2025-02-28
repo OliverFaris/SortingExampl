9e174179b48660109c510ae8e9449a066e8b245f
oliverfaris <oliver.faris@menloschool.org>
2025-02-28 11:50:41 -0800

    Did bubble sort, and selection sort a bit

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -15,11 +15,33 @@ public class SortingAlgorithms {
     // Bubble Sort
     public static void bubbleSort(int[] arr) {
         // TODO: Implement bubble sort
+        int n = arr.length;
+        int temp = arr[0];
+        for (int i = 0; i < n -1; i++) {
+            for (int j = 0; j < n - i-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
     }
 
     // Selection Sort
     public static void selectionSort(int[] arr) {
         // TODO: Implement selection sort
+        int temp = -1;
+        for (int i = 0; i < arr.length -1; i++) {
+            int smallest = 0;
+            for (int j = 0; j < arr.length -i -1; j++) {
+                if (arr[j] < arr[smallest])
+                    smallest = j;
+            }
+            temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
     }
 
     // Merge Sort
